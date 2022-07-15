24ac90f7ef98e2f93c2c48ccc1ca8919ba80649f
ramkumarsync <ramkumarr@syncfusion.com>
2022-07-15 13:40:19 +0530

    Added sample application for annotations list view

---
+++ AnnotationsListView/MainPage.xaml.cs
@@ -0,0 +1,77 @@
+﻿using Syncfusion.DataSource;
+using Syncfusion.SfPdfViewer.XForms;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AnnotationsListView
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            hamburgerButton.ImageSource = (FileImageSource)ImageSource.FromFile("hamburgericon.png");
+            listView.DataSource.GroupDescriptors.Add(new GroupDescriptor()
+            {
+                PropertyName = "PageNumber",
+                KeySelector = (object obj) =>
+                {
+                    var item = (obj as IAnnotation);
+                    return "PAGE " + item.PageNumber;
+                },
+            });
+        }
+
+        private void hamburgerButton_Clicked(object sender, EventArgs e)
+        {
+            navigationDrawer.ToggleDrawer();
+        }
+
+        private void listView_ItemTapped(object sender, Syncfusion.ListView.XForms.ItemTappedEventArgs e)
+        {
+            double x = 0;
+            double y = 0;
+
+            if (e.ItemType != Syncfusion.ListView.XForms.ItemType.GroupHeader)
+            {
+                pdfViewer.AnnotationMode = AnnotationMode.None;
+
+                IAnnotation annotation = e.ItemData as IAnnotation;
+                pdfViewer.SelectAnnotation(annotation);                
+
+                if (annotation is ShapeAnnotation shape)
+                {
+                    x = shape.Bounds.X;
+                    y = shape.Bounds.Y;
+                }
+                else if (annotation is TextMarkupAnnotation textMarkup)
+                {
+                    x = textMarkup.Bounds[0].X;
+                    y = textMarkup.Bounds[0].Y;
+                }
+                else if (annotation is FreeTextAnnotation freeText)
+                {
+                    x = freeText.Bounds.X;
+                    y = freeText.Bounds.Y;
+                }
+                else if (annotation is InkAnnotation ink)
+                {
+                    x = ink.Bounds.X;
+                    y = ink.Bounds.Y;
+                }
+
+                var points = pdfViewer.ConvertPagePointToScrollPoint(new Point(x, y), annotation.PageNumber);
+                pdfViewer.VerticalOffset = (float)points.Y - 30;
+
+                navigationDrawer.ToggleDrawer();
+            }
+
+        }
+    }
+}
---
+++ AnnotationsListView/Views/ListViewItem.xaml.cs
@@ -0,0 +1,95 @@
+﻿using Syncfusion.SfPdfViewer.XForms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace AnnotationsListView
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ListViewItem : ContentView
+    {
+        public ListViewItem()
+        {
+            InitializeComponent();
+            BindingContextChanged += ListViewItem_BindingContextChanged;
+        }
+
+        private void ListViewItem_BindingContextChanged(object sender, EventArgs e)
+        {
+            if (BindingContext == null)
+                return;
+
+            annotationFontIcon.FontFamily = FontMappingHelper.FontFamily;
+
+            if (BindingContext is ShapeAnnotation annotation)
+            {
+                annotationLabel.Text = annotation.ShapeAnnotationType.ToString();
+
+                if (annotation.ShapeAnnotationType == ShapeAnnotationType.Line)
+                {
+                    annotationFontIcon.Text = FontMappingHelper.Line;
+                }
+                else if (annotation.ShapeAnnotationType == ShapeAnnotationType.Rectangle)
+                {
+                    annotationFontIcon.Text = FontMappingHelper.Rectangle;
+                }
+                else if (annotation.ShapeAnnotationType == ShapeAnnotationType.Circle)
+                {
+                    annotationFontIcon.Text = FontMappingHelper.Circle;
+                }
+                else if (annotation.ShapeAnnotationType == ShapeAnnotationType.Arrow)
+                {
+                    annotationFontIcon.Text = FontMappingHelper.Arrow;
+                }
+                else if (annotation.ShapeAnnotationType == ShapeAnnotationType.Polygon)
+                {
+                    annotationFontIcon.Text = FontMappingHelper.Polygon;
+                }
+            }
+            else if (BindingContext is TextMarkupAnnotation textMarkup)
+            {
+                annotationLabel.Text = textMarkup.TextMarkupAnnotationType.ToString();
+                if (textMarkup.TextMarkupAnnotationType == TextMarkupAnnotationType.Highlight)
+                {
+                    annotationFontIcon.Text = FontMappingHelper.Highlight;
+                }
+                else if (textMarkup.TextMarkupAnnotationType == TextMarkupAnnotationType.Strikethrough)
+                {
+                    annotationFontIcon.Text = FontMappingHelper.StrikeThrough;
+                }
+                else if (textMarkup.TextMarkupAnnotationType == TextMarkupAnnotationType.Underline)
+                {
+                    annotationFontIcon.Text = FontMappingHelper.Underline;
+                }
+            }
+            else if (BindingContext is FreeTextAnnotation freeText)
+            {
+                annotationLabel.Text = "Free Text";
+                annotationFontIcon.Text = FontMappingHelper.EditText;
+            }
+            else if (BindingContext is InkAnnotation ink)
+            {
+                annotationLabel.Text = "Ink";
+                annotationFontIcon.Text = FontMappingHelper.Ink;
+            }
+
+            //else if (BindingContext is StampAnnotation stamp)
+            //{
+            //    annotationLabel.Text = $"{ToString(stamp)}";
+            //    annotationFontIcon.FontFamily = FontMappingHelper.StampFont;
+            //    annotationFontIcon.Text = FontMappingHelper.Stamp;
+            //}
+            //else if (BindingContext is HandwrittenSignature signature)
+            //{
+            //    annotationLabel.Text = $"{ToString(signature)}";
+            //    annotationFontIcon.FontFamily = FontMappingHelper.SignatureFont;
+            //    annotationFontIcon.Text = FontMappingHelper.Signature;
+            //}
+        }
+    }
+}
